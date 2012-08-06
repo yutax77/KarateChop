f9d24d8ef03e105f29dac23ef7ff3691e56986f4
yutax77 <yutax77@gmail.com>
2012-08-06 17:07:41 +0900

    Project setting.

--- project/KaratechopBuild.scala
+++ project/KaratechopBuild.scala
@@ -2,6 +2,10 @@ import sbt._
 import sbt.Keys._
 
 object KaratechopBuild extends Build {
+  val scalaTest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+  lazy val dependencies = Seq(
+    scalaTest
+  )
 
   lazy val karatechop = Project(
     id = "karatechop",
@@ -10,8 +14,9 @@ object KaratechopBuild extends Build {
       name := "KarateChop",
       organization := "com.yutax77",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.9.2"
+      scalaVersion := "2.9.2",
       // add other settings here
+      libraryDependencies ++= dependencies
     )
   )
 }
