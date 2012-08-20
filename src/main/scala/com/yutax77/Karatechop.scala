faac47c6475c602f3fa744b20f3b740d23a5a25e
yutax77 <yutax77@gmail.com>
2012-08-20 22:23:22 +0900

    Impl1

--- src/main/scala/com/yutax77/Karatechop.scala
+++ src/main/scala/com/yutax77/Karatechop.scala
@@ -2,6 +2,26 @@ package com.yutax77
 
 object KarateChop {
     def chop(target:Int, array:Array[Int]):Int = {
-        return -1
+        if(array.isEmpty)
+          return -1
+
+        return chopImpl(target, array, 0, array.size)
+     }
+
+    private def chopImpl(target:Int, array:Array[Int], start:Int, end:Int):Int = {
+        if(start == end)
+          return -1
+
+        val pivot = (end - start) / 2
+        val value = array(pivot)
+        if(target == value){
+            return pivot
+        }
+        else if(target > value){
+            return chopImpl(target, array, pivot, array.size)
+        }
+        else{
+            return chopImpl(target, array, 0, pivot)
+        }
     }
 }
